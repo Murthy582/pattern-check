6e56d23596245081375a8c5962aeae45ed56e649
Murthy VVR <murthy.582@gmail.com>
2015-09-24 15:42:10 +0530

    Swift code changes

--- src/ios/PatternCheck.swift
+++ src/ios/PatternCheck.swift
@@ -1,10 +1,10 @@
 import Foundation
 
  @objc(PCheck) class PatternCheck : CDVPlugin {
-    func greet(command: CDVInvokedUrlCommand) {
+    func isPatternEnabled(command: CDVInvokedUrlCommand) {
         //var message = command.arguments[0] as String
         
         var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "Hello Murthy !")
-        commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)
+        commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
     }
 }
